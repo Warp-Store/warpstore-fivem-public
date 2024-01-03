489dda645f30e4efdb161637e083d3ed20d8fb1d
Rabbit <personal.danielsilva.d3v@outlook.com>
2024-01-03 19:06:48 -0300

    Update warpstore.client.lua

--- src/warpstore.client.lua
+++ src/warpstore.client.lua
@@ -1,10 +1,10 @@
 RegisterNetEvent('warpShowMessage')
 AddEventHandler('warpShowMessage', function(title, subTitle)
     SendNUIMessage({title = title, subTitle = subTitle, resourceName =  GetCurrentResourceName()});
-    SetNuiFocus(true, false)
+    --SetNuiFocus(true, false)
 end)
 
 RegisterNUICallback('removeFocus', function(data, cb)
-    SetNuiFocus(false, false)
+    --SetNuiFocus(false, false)
     cb(true)
 end)
