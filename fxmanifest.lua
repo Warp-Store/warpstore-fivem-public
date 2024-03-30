92f66ec7400d66a75ab3043ffc67db1c5857ef12
Rabbit <personal.danielsilva.d3v@outlook.com>
2024-03-30 17:31:29 -0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 ui_page 'https://storage.warpstore.app/plugins/fivem/web/index.html'
 
-version '2.0.1'
+version '2.0.2'
 
 client_scripts { 'src/warpstore.client.js' }
 server_scripts { '@vrp/lib/utils.lua', 'src/warpstore.server.js' }
